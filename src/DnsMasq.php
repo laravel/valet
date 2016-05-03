925bc30cb2a2ed975c9ce12d9560b19d7a256778
Taylor Otwell <taylorotwell@gmail.com>
2016-05-03 16:09:49 -0500

    tweak dnsmasq installation

--- src/DnsMasq.php
+++ src/DnsMasq.php
@@ -72,9 +72,7 @@ class DnsMasq
     {
         $dnsMasqConfigPath = '/Users/'.$_SERVER['SUDO_USER'].'/.malt/dnsmasq.conf';
 
-        if (! file_exists('/usr/local/etc/dnsmasq.conf')) {
-            copy('/usr/local/opt/dnsmasq/dnsmasq.conf.example', '/usr/local/etc/dnsmasq.conf');
-        }
+        copy('/usr/local/opt/dnsmasq/dnsmasq.conf.example', '/usr/local/etc/dnsmasq.conf');
 
         if (strpos(file_get_contents('/usr/local/etc/dnsmasq.conf'), $dnsMasqConfigPath) === false) {
             file_put_contents('/usr/local/etc/dnsmasq.conf', PHP_EOL.'conf-file='.$dnsMasqConfigPath.PHP_EOL, FILE_APPEND);
@@ -94,10 +92,6 @@ class DnsMasq
      */
     protected static function createResolver()
     {
-        if (! file_exists('/etc/resolver/dev')) {
-            file_put_contents('/etc/resolver/dev', 'nameserver 127.0.0.1'.PHP_EOL);
-        } elseif (trim(file_get_contents('/etc/resolver/dev')) != 'nameserver 127.0.0.1') {
-            throw new Exception('A /etc/resolver/dev file already exists. Unable to configure DnsMasq.');
-        }
+        file_put_contents('/etc/resolver/dev', 'nameserver 127.0.0.1'.PHP_EOL);
     }
 }
