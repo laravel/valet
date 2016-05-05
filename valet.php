a8e7ac7c0ab9219946ecbd6154104ca8444fed33
Taylor Otwell <taylorotwell@gmail.com>
2016-05-05 10:06:51 -0500

    version

--- valet.php
+++ valet.php
@@ -16,7 +16,7 @@ use Silly\Application;
 /**
  * Create the application.
  */
-$app = new Application('Laravel Valet', 'v1.0.3');
+$app = new Application('Laravel Valet', 'v1.0.4');
 
 /**
  * Prune missing directories and symbolic links on every command.
