e00b65709de3f274ddbc57fbf970d8aa9482da78
Igor Pantovic <php.igor@gmail.com>
2016-05-06 01:14:02 +0200

    Symfony2/3 Valet driver
    This driver should work for both Symfony V2 and V3.

---
+++ drivers/SymfonyValetDriver.php
@@ -0,0 +1,49 @@
+<?php
+
+class SymfonyValetDriver extends ValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return file_exists($sitePath.'/web/app_dev.php') &&
+               (file_exists($sitePath.'/bin/symfony_requirements') ||
+                file_exists($sitePath.'/app/SymfonyRequirements.php'));
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        if (file_exists($staticFilePath = $sitePath.'/web/'.$uri)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        return $sitePath.'/web/app_dev.php';
+    }
+}
