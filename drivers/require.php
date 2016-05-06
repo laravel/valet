e7b2e32196c1714a78fa28151505f0a92da43878
Tristan Payne <tpayne@emfluence.com>
2016-05-06 12:08:19 -0500

    Add support for Drupal

--- drivers/require.php
+++ drivers/require.php
@@ -6,6 +6,7 @@ require_once __DIR__.'/LaravelValetDriver.php';
 require_once __DIR__.'/StaticValetDriver.php';
 require_once __DIR__.'/JigsawValetDriver.php';
 require_once __DIR__.'/WordPressValetDriver.php';
+require_once __DIR__.'/DrupalValetDriver.php';
 require_once __DIR__.'/CraftValetDriver.php';
 require_once __DIR__.'/SymfonyValetDriver.php';
 require_once __DIR__.'/GenericPhpValetDriver.php';
