b2624d48bc3bb9b855b21c69294ad5df1de8f1d3
Taylor Otwell <taylorotwell@gmail.com>
2016-05-05 20:07:52 -0500

    add files

---
+++ drivers/require.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__.'/ValetDriver.php';
+require_once __DIR__.'/StatamicValetDriver.php';
+require_once __DIR__.'/LaravelValetDriver.php';
+require_once __DIR__.'/StaticValetDriver.php';
+require_once __DIR__.'/JigsawValetDriver.php';
+require_once __DIR__.'/WordPressValetDriver.php';
+require_once __DIR__.'/CraftValetDriver.php';
