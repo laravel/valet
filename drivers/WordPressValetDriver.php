e46cd292f4fee2d8333e16a739fe7e027b64e147
Taylor Otwell <taylorotwell@gmail.com>
2016-05-06 14:39:26 -0500

    wordpress for later purposes

---
+++ drivers/WordPressValetDriver.php
@@ -0,0 +1,17 @@
+<?php
+
+class WordPressValetDriver extends BasicValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return void
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return is_dir($sitePath.'/wp-admin');
+    }
+}
