8b20e11c6c5bee3ddfdc0dc2a6d39f618e4bfd51
Jason Walton <jwalton512@gmail.com>
2016-05-06 12:17:30 -0700

    Add Sculpin Valet driver

--- drivers/require.php
+++ drivers/require.php
@@ -9,3 +9,4 @@ require_once __DIR__.'/WordPressValetDriver.php';
 require_once __DIR__.'/CraftValetDriver.php';
 require_once __DIR__.'/SymfonyValetDriver.php';
 require_once __DIR__.'/GenericPhpValetDriver.php';
+require_once __DIR__.'/SculpinValetDriver.php';
