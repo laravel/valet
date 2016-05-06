907ef8c7047fdb488f7978ff22921fe253536b05
Nil Portugues Caldero <nil@letgo.com>
2016-05-06 14:02:50 +0200

    Adding fallback for PBCOPY

--- fetch-share-url.sh
+++ fetch-share-url.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PBCOPY=$(which pbcopy)
 
-php $DIR/valet.php fetch-share-url | pbcopy
+if [ -z "$PBCOPY" ]; then
+    PBCOPY='xsel --clipboard --input'
+fi
+
+php $DIR/valet.php fetch-share-url | $PBCOPY
