fc742f34a9c1c7c43cb4497f4fa50f48cec164bd
Taylor Otwell <taylorotwell@gmail.com>
2016-05-06 23:18:35 -0500

    a few php fpm fixes

--- server.php
+++ server.php
@@ -6,11 +6,6 @@
 define('VALET_HOME_PATH', '/Users/'.posix_getpwuid(fileowner(__FILE__))['name'].'/.valet');
 
 /**
- * De-escalate root privileges down to Valet directory owner.
- */
-posix_setuid(fileowner(VALET_HOME_PATH.'/config.json'));
-
-/**
  * Show the Valet 404 "Not Found" page.
  */
 function show_valet_404()
