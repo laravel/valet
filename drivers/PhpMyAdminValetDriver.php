816d999059ba1e11a37b35d8a8581efe41f2aa00
BePsvPT <freedom@csie.io>
2016-05-08 02:38:01 +0800

    Add phpMyAdmin driver

---
+++ drivers/PhpMyAdminValetDriver.php
@@ -0,0 +1,54 @@
+<?php
+
+class PhpMyAdminValetDriver extends ValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return void
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return file_exists($sitePath.'/phpmyadmin.css.php') &&
+            file_exists($sitePath.'/index.php');
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        if (file_exists($sitePath.$uri) &&
+            ! is_dir($sitePath.$uri) &&
+            pathinfo($sitePath.$uri)['extension'] != 'php') {
+            return $sitePath.$uri;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        if ('/' === $uri) {
+            return $sitePath.'/index.php';
+        }
+
+        return $sitePath.$uri;
+    }
+}
