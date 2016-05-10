bcb45d6aabb51374c59bf9cdcb724eef7f8e263e
Benjamin Milde <benni@kobrakai.de>
2016-05-10 20:54:51 +0200

    Added ProcessWire Driver

--- cli/drivers/ValetDriver.php
+++ cli/drivers/ValetDriver.php
@@ -57,6 +57,7 @@ abstract class ValetDriver
         $drivers[] = 'KirbyValetDriver';
         $drivers[] = 'ContaoValetDriver';
         $drivers[] = 'KatanaValetDriver';
+        $drivers[] = 'ProcessWireValetDriver';
 
         $drivers[] = 'BasicValetDriver';
 
