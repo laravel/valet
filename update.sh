70055c2f39a89f89ba7644e18c2437c2d8660a8f
Taylor Otwell <taylorotwell@gmail.com>
2016-05-10 14:05:04 -0500

    adding note to update script

--- update.sh
+++ update.sh
@@ -5,7 +5,8 @@ rm -rf $HOME/.valet-cli
 
 # Download and unpack the latest Valet release
 mkdir $HOME/.valet-cli
-wget https://github.com/laravel/valet/archive/master.tar.gz -O $HOME/.valet-cli/valet.tar.gz
+echo "Downloading latest release of Valet... ☁️"
+wget https://github.com/laravel/valet/archive/master.tar.gz -O $HOME/.valet-cli/valet.tar.gz > /dev/null 2>&1
 tar xvzf $HOME/.valet-cli/valet.tar.gz -C $HOME/.valet-cli --strip 1 > /dev/null 2>&1
 
 # Install Valet's Composer dependencies
