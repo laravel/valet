0e4e81f9292aa969e10e61d20228887f87b2ea92
Taylor Otwell <taylorotwell@gmail.com>
2016-05-10 15:59:25 -0500

    fix autoload

--- cli/valet.php
+++ cli/valet.php
@@ -7,7 +7,7 @@
 if (file_exists(__DIR__.'/../vendor/autoload.php')) {
     require __DIR__.'/../vendor/autoload.php';
 } else {
-    require __DIR__.'/../../autoload.php';
+    require __DIR__.'/../../../autoload.php';
 }
 
 use Silly\Application;
