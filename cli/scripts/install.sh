6345ee950338ee141e7b1c23492bde03c85f3f06
Taylor Otwell <taylorotwell@gmail.com>
2016-05-10 15:06:29 -0500

    move scripts

--- cli/scripts/install.sh
+++ cli/scripts/install.sh
@@ -19,14 +19,14 @@ brew install php70
 # Install Composer to /usr/local/bin
 if [[ ! $(which composer -A) ]]
 then
-echo "Installing Composer..."
-/usr/local/bin/php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-/usr/local/bin/php -r "if (hash_file('SHA384', 'composer-setup.php') === '92102166af5abdb03f49ce52a40591073a7b859a86e8ff13338cf7db58a19f7844fbc0bb79b2773bf30791e935dbd938') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" > /dev/null 2>&1
-/usr/local/bin/php composer-setup.php > /dev/null 2>&1
-/usr/local/bin/php -r "unlink('composer-setup.php');"
+    echo "Installing Composer..."
+    /usr/local/bin/php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    /usr/local/bin/php -r "if (hash_file('SHA384', 'composer-setup.php') === '92102166af5abdb03f49ce52a40591073a7b859a86e8ff13338cf7db58a19f7844fbc0bb79b2773bf30791e935dbd938') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" > /dev/null 2>&1
+    /usr/local/bin/php composer-setup.php > /dev/null 2>&1
+    /usr/local/bin/php -r "unlink('composer-setup.php');"
 
-mv composer.phar /usr/local/bin/composer
-chmod +x /usr/local/bin/composer
+    mv composer.phar /usr/local/bin/composer
+    chmod +x /usr/local/bin/composer
 fi
 
 COMPOSER_PATH=$(which composer)
