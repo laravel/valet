b745161f2fe0cf47c05f2341b37f812ef6a012af
Taylor Otwell <taylorotwell@gmail.com>
2016-05-11 13:31:33 -0500

    version

--- cli/valet.php
+++ cli/valet.php
@@ -28,7 +28,7 @@ use Illuminate\Container\Container;
  */
 Container::setInstance(new Container);
 
-$app = new Application('Laravel Valet', 'v1.1.4');
+$app = new Application('Laravel Valet', 'v1.1.5');
 
 /**
  * Prune missing directories and symbolic links on every command.
