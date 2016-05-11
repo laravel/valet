293c7c6e61f86b88435cdbbe19f7436a4e46b7ff
corneliusio <guy@cornelius.io>
2016-05-11 11:27:09 -0500

    account for wp installs using fancyguy/webroot-installer

--- cli/drivers/WordpressComposerInstallValetDriver.php
+++ cli/drivers/WordpressComposerInstallValetDriver.php
@@ -14,7 +14,8 @@ class WordpressComposerInstallValetDriver extends ValetDriver
     {
         if (file_exists($sitePath . '/composer.json')
             && (file_exists($sitePath . '/wp-config.php')
-                || is_dir($sitePath . '/vendor/johnpbloch/wordpress-core-installer'))
+                || $this->isJohnpblochInstall($sitePath)
+                || $this->isWebrootInstall($sitePath))
         ) {
             return true;
         }
@@ -37,8 +38,16 @@ class WordpressComposerInstallValetDriver extends ValetDriver
             $sitePath .= '/web';
         }
 
-        if (file_exists($staticFilePath = $sitePath . $uri) && !$this->fileIsPHP($uri) && !is_dir($staticFilePath)) {
-            return $staticFilePath;
+        if ($this->getStaticFilePath($sitePath, $uri)) {
+            return $this->getStaticFilePath($sitePath, $uri);
+        }
+
+        if ($this->isWebrootInstall($sitePath)) {
+            $sitePath = $this->getWebrootInstallPath($sitePath);
+
+            if ($this->getStaticFilePath($sitePath, $uri)) {
+                return $this->getStaticFilePath($sitePath, $uri);
+            }
         }
 
         return false;
@@ -59,14 +68,17 @@ class WordpressComposerInstallValetDriver extends ValetDriver
             $sitePath .= '/web';
         }
 
-        if (is_dir($staticFilePath = $sitePath . $uri)) {
-            return $staticFilePath . '/index.php';
-        } elseif ($this->fileIsPHP($uri)) {
-            $_SERVER['PHP_SELF'] = $uri;
-            return $sitePath . preg_replace('/\/$/', '', $uri);
+        if ($this->getFrontControllerPath($sitePath, $uri)) {
+            return $this->getFrontControllerPath($sitePath, $uri);
         }
 
-        return $sitePath . '/index.php';
+        if ($this->isWebrootInstall($sitePath)) {
+            $webrootSitePath = $this->getWebrootInstallPath($sitePath);
+
+            if ($this->getFrontControllerPath($webrootSitePath, $uri)) {
+                return $this->getFrontControllerPath($webrootSitePath, $uri);
+            }
+        }
     }
 
     /**
@@ -81,6 +93,77 @@ class WordpressComposerInstallValetDriver extends ValetDriver
                 && file_exists($path . '/web/wp-config.php')
                 && file_exists($path . '/config/application.php'))
         );
+    }
+
+    /**
+     * Checks if current install is using [fancyguy/webroot-installer](https://github.com/fancyguy/webroot-installer)
+     * @param  string  $path
+     * @return boolean
+     */
+    private function isJohnpblochInstall($path)
+    {
+        return (is_dir($path . '/vendor/johnpbloch/wordpress-core-installer'));
+    }
+
+    /**
+     * Checks if current install is using [fancyguy/webroot-installer](https://github.com/fancyguy/webroot-installer)
+     * @param  string  $path
+     * @return boolean
+     */
+    private function isWebrootInstall($path)
+    {
+        return (is_dir($path . '/vendor/fancyguy/webroot-installer'));
+    }
+
+    /**
+     * parses composer.json for the install path defined for fancyguy/webroot-installer
+     * @param  string $path
+     * @return string
+     */
+    private function getWebrootInstallPath($path)
+    {
+        $composer = json_decode(file_get_contents($path . '/composer.json'), true);
+        return $path .= '/'. $composer['extra']['webroot-dir'];
+    }
+
+    /**
+     * Helper function to determine whether to serve a static file.
+     * @param  string $path
+     * @param  string $uri
+     * @return string|bool
+     */
+    private function getStaticFilePath($path, $uri)
+    {
+        if (file_exists($staticPath = $path . $uri) && !$this->fileIsPHP($uri) && !is_dir($staticPath)) {
+            return $staticPath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Helper function to determine what file should process request.
+     * @param  string $path
+     * @param  string $uri
+     * @return string|bool
+     */
+    private function getFrontControllerPath($path, $uri)
+    {
+        // If request is for real directory, check for index.php (e.g. /wp-admin/)
+        if (is_dir($staticPath = $path . $uri) && file_exists($indexedStaticPath = $staticPath . '/index.php')) {
+            return $indexedStaticPath;
+
+        // If request is for specific php file, use it if it exists (e.g. /wp-admin/edit.php, /wp-admin/upload.php)
+        } elseif ($this->fileIsPHP($uri) && file_exists($phpFile = $path . $uri)) {
+            $_SERVER['PHP_SELF'] = $uri;
+            return preg_replace('/\/$/', '', $phpFile);
+
+        // If nothing else, check the provided site path for an index.php (i.e. lets WP rewrite rules handle things)
+        } elseif (file_exists($rootIndex = $path . '/index.php')) {
+            return $rootIndex;
+        }
+
+        return false;
     }
 
     /**
