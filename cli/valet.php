2ea59d73d35dec6b5ac2c85fef8691fbb335d7b6
Taylor Otwell <taylorotwell@gmail.com>
2016-05-13 12:37:40 -0500

    version

--- cli/valet.php
+++ cli/valet.php
@@ -18,7 +18,7 @@ use Illuminate\Container\Container;
  */
 Container::setInstance(new Container);
 
-$version = '1.1.9';
+$version = '1.1.10';
 
 $app = new Application('Laravel Valet', $version);
 
