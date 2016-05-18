c86f6b563f0c6eba14cc064bc811544e9b5504bc
Ramon Villain <ramon.villain@gmail.com>
2016-05-18 13:58:48 -0300

    Create ThemosisValetDriver.php

---
+++ cli/drivers/ThemosisValetDriver.php
@@ -0,0 +1,43 @@
+<?php
+
+class ThemosisValetDriver extends BasicValetDriver
+{
+    /**
+     * Mutate the incoming URI.
+     *
+     * @param  string  $uri
+     * @return string
+     */
+    public function mutateUri($uri)
+    {
+        return rtrim('/htdocs'.$uri, '/');
+    }
+
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return is_dir($sitePath.'/htdocs');
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        $_SERVER['PHP_SELF'] = $uri;
+
+        return parent::frontControllerPath($sitePath, $siteName, $uri);
+    }
+}
