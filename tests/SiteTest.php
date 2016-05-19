032f37e6e6d19308c733f479ac292fe3b1dc774a
Lucas Michot <lucas@semalead.com>
2016-05-19 21:12:42 +0200

    Use assertFileExists and assertFileNotExists

--- tests/SiteTest.php
+++ tests/SiteTest.php
@@ -37,7 +37,7 @@ class SiteTest extends PHPUnit_Framework_TestCase
         swap(Configuration::class, $config);
 
         $linkPath = resolve(Site::class)->link('target', 'link');
-        $this->assertEquals(VALET_HOME_PATH.'/Sites/link', $linkPath);
+        $this->assertSame(VALET_HOME_PATH.'/Sites/link', $linkPath);
     }
 
 
@@ -47,11 +47,11 @@ class SiteTest extends PHPUnit_Framework_TestCase
         symlink(__DIR__.'/output/file.out', __DIR__.'/output/link');
         $site = resolve(StubForRemovingLinks::class);
         $site->unlink('link');
-        $this->assertFalse(file_exists(__DIR__.'/output/link'));
+        $this->assertFileNotExists(__DIR__.'/output/link');
 
         $site = resolve(StubForRemovingLinks::class);
         $site->unlink('link');
-        $this->assertFalse(file_exists(__DIR__.'/output/link'));
+        $this->assertFileNotExists(__DIR__.'/output/link');
     }
 
 
@@ -62,14 +62,14 @@ class SiteTest extends PHPUnit_Framework_TestCase
         unlink(__DIR__.'/output/file.out');
         $site = resolve(StubForRemovingLinks::class);
         $site->pruneLinks();
-        $this->assertFalse(file_exists(__DIR__.'/output/link'));
+        $this->assertFileNotExists(__DIR__.'/output/link');
     }
 
 
     public function test_logs_method_returns_array_of_log_files()
     {
         $logs = resolve(Site::class)->logs([__DIR__.'/test-directory-for-logs']);
-        $this->assertEquals(__DIR__.'/test-directory-for-logs/project/storage/logs/laravel.log', $logs[0]);
+        $this->assertSame(__DIR__.'/test-directory-for-logs/project/storage/logs/laravel.log', $logs[0]);
         unlink(__DIR__.'/test-directory-for-logs/project/storage/logs/laravel.log');
     }
 }
