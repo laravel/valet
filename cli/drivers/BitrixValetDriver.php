178797fbd9a81db7c49d349b1fe60da50c030967
Sergey Antsupov <antsupov.sa@icloud.com>
2016-05-23 10:51:19 +0300

    add driver for 1C-Bitrix CMS

---
+++ cli/drivers/BitrixValetDriver.php
@@ -0,0 +1,67 @@
+<?php
+
+class BitrixValetDriver extends BasicValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return is_dir($sitePath.'/bitrix');
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        if (file_exists($staticFilePath = $sitePath.'/'.$uri)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    /*public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        $_SERVER['PHP_SELF'] = $uri;
+        $_SERVER["DOCUMENT_ROOT"] = $sitePath;
+        $_SERVER['SCRIPT_FILENAME'] =  $sitePath.'/company/personal.php';
+        return parent::frontControllerPath($sitePath, $siteName, $uri);
+    }*/
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        $_SERVER["DOCUMENT_ROOT"] = $sitePath;
+
+        $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
+
+        return $sitePath.'/bitrix/urlrewrite.php';
+    }
+}
