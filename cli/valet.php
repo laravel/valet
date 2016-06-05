7b69564c6726b1591916ae9d428eef75e99ef73d
Carlos Priego <carlos.enrique.priego@gmail.com>
2016-06-05 15:24:03 -0500

    version 1.1.13

--- cli/valet.php
+++ cli/valet.php
@@ -18,7 +18,7 @@ use Illuminate\Container\Container;
  */
 Container::setInstance(new Container);
 
-$version = '1.1.12';
+$version = '1.1.13';
 
 $app = new Application('Laravel Valet', $version);
 
