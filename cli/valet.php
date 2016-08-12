b175fe2890c5ce9e1d80bd259b128acafc0120f1
Jason Conroy <jason@findingsimple.com>
2016-08-12 10:28:23 +1000

    Update version to match release

--- cli/valet.php
+++ cli/valet.php
@@ -18,7 +18,7 @@ use Illuminate\Container\Container;
  */
 Container::setInstance(new Container);
 
-$version = '1.1.16';
+$version = '1.1.17';
 
 $app = new Application('Laravel Valet', $version);
 
