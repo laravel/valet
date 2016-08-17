86d0c6b1a75a6fb80380d2ddaba71935680045d7
Adam Wathan <adam.wathan@gmail.com>
2016-08-17 17:08:46 -0400

    Increment version

--- cli/valet.php
+++ cli/valet.php
@@ -18,7 +18,7 @@ use Illuminate\Container\Container;
  */
 Container::setInstance(new Container);
 
-$version = '1.1.18';
+$version = '1.1.19';
 
 $app = new Application('Laravel Valet', $version);
 
