c1eedb402104b6e55cec7be5569727618ca2d72f
Leonardo Nodari <nodarileonardo@gmail.com>
2017-02-02 16:27:25 +0100

    Warning on SELinux in enforcing mode

--- cli/Valet/Warning.php
+++ cli/Valet/Warning.php
@@ -4,12 +4,25 @@ namespace Valet;
 
 class Warning
 {
+    var $cli;
+
+    /**
+     * Create a new Warning instance.
+     *
+     * @param CommandLine $cli
+     */
+    public function __construct(CommandLine $cli)
+    {
+        $this->cli = $cli;
+    }
+
     /**
      * Run all checks and output warnings.
      */
     function check()
     {
         $this->homePathIsInsideRoot();
+        $this->seLinuxIsEnabled();
     }
 
     /**
@@ -22,6 +35,20 @@ class Warning
     {
         if (strpos(VALET_HOME_PATH, '/root/') === 0) {
             warning("⚠️️ Valet home directory is inside /root!");
+        }
+    }
+
+    /**
+     * Verify is SELinux is enabled and in enforcing mode.
+     */
+    function seLinuxIsEnabled()
+    {
+        $output = $this->cli->run('sestatus');
+
+        if (preg_match('@SELinux status:(\s+)enabled@', $output)
+            && preg_match('@Current mode:(\s+)enforcing@', $output)
+        ) {
+            warning("⚠️️ SELinux is enabled!");
         }
     }
 }
