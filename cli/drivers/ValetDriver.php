bc930fc9cfaea9b451db2f720950bc51c001ae31
Michael Gruschwitz <info@grusch-it.de>
2017-04-21 17:18:25 +0200

    Fix contao driver never gets loaded
    The symfony valet driver should not override the contao valet driver.

--- cli/drivers/ValetDriver.php
+++ cli/drivers/ValetDriver.php
@@ -54,6 +54,7 @@ abstract class ValetDriver
 
         $drivers[] = 'WordPressValetDriver';
         $drivers[] = 'BedrockValetDriver';
+        $drivers[] = 'ContaoValetDriver';
         $drivers[] = 'SymfonyValetDriver';
         $drivers[] = 'CraftValetDriver';
         $drivers[] = 'StatamicValetDriver';
@@ -62,7 +63,6 @@ abstract class ValetDriver
         $drivers[] = 'SculpinValetDriver';
         $drivers[] = 'JigsawValetDriver';
         $drivers[] = 'KirbyValetDriver';
-        $drivers[] = 'ContaoValetDriver';
         $drivers[] = 'KatanaValetDriver';
         $drivers[] = 'JoomlaValetDriver';
         $drivers[] = 'DrupalValetDriver';
