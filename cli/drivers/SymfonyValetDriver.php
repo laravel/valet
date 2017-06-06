dbcbf1edc799e2901c136eea475553666dff341d
Vic D'Elfant <vic@wijzijnmiddag.nl>
2017-06-06 16:10:41 +0200

    Improved HTTP_HOST rewriting

--- cli/drivers/SymfonyValetDriver.php
+++ cli/drivers/SymfonyValetDriver.php
@@ -45,6 +45,10 @@ class SymfonyValetDriver extends ValetDriver
      */
     public function frontControllerPath($sitePath, $siteName, $uri)
     {
+        if (isset($_SERVER['HTTP_X_ORIGINAL_HOST']) && isset($_SERVER['HTTP_X_INBOUND_HOST']) && $_SERVER['HTTP_X_ORIGINAL_HOST'] === $_SERVER['HTTP_HOST']) {
+            $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_INBOUND_HOST'];
+        }
+
         if (file_exists($frontControllerPath = $sitePath.'/web/app_dev.php')) {
             return $frontControllerPath;
         } elseif (file_exists($frontControllerPath = $sitePath.'/web/app.php')) {
