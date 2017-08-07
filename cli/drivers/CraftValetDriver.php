c8c1a52c3bf84ca5fbfa46222e941c477a51471b
Erik Weber <terbolous@gmail.com>
2017-08-07 09:52:50 +0200

    Add $_SERVER['DOCUMENT_ROOT'] to CraftValetDriver

--- cli/drivers/CraftValetDriver.php
+++ cli/drivers/CraftValetDriver.php
@@ -191,6 +191,7 @@ class CraftValetDriver extends ValetDriver
         $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
         $_SERVER['SCRIPT_NAME'] = $scriptName;
         $_SERVER['PHP_SELF'] = $scriptName;
+        $_SERVER['DOCUMENT_ROOT'] = $sitePath.'/'.$frontControllerDirectory;
 
         return $indexPath;
     }
