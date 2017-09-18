18a6a519f17ad34f8ab60e781eaed7f0187e7ec3
Lasse Rafn <lasserafn@gmail.com>
2017-09-18 14:27:17 +0200

    Update NginxTest.php
    .localhost default domain test NginxTest

--- tests/NginxTest.php
+++ tests/NginxTest.php
@@ -78,13 +78,13 @@ class NginxTest extends PHPUnit_Framework_TestCase
         $files->shouldReceive('putAsUser')->with(VALET_HOME_PATH.'/Nginx/.keep', "\n")->once();
 
         swap(Filesystem::class, $files);
-        swap(Configuration::class, $config = Mockery::spy(Configuration::class, ['read' => ['domain' => 'dev']]));
+        swap(Configuration::class, $config = Mockery::spy(Configuration::class, ['read' => ['domain' => 'localhost']]));
         swap(Site::class, $site = Mockery::spy(Site::class));
 
         $nginx = resolve(Nginx::class);
         $nginx->installNginxDirectory();
 
-        $site->shouldHaveReceived('resecureForNewDomain', ['dev', 'dev']);
+        $site->shouldHaveReceived('resecureForNewDomain', ['localhost', 'localhost']);
     }
 
 }
