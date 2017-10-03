dc48a23dfa4f112ffa64f8aa8ab9066eea4aebd2
Tim Neutkens <tim@weprovide.com>
2017-10-03 16:33:18 -0600

    Port improvements for Magento2 driver from Valet Plus project

--- cli/drivers/Magento2ValetDriver.php
+++ cli/drivers/Magento2ValetDriver.php
@@ -2,25 +2,30 @@
 
 class Magento2ValetDriver extends ValetDriver
 {
-
-    /**
-     * Holds the MAGE_MODE from app/etc/config.php or $ENV
-     *
-     * @var string
-     */
-    private $mageMode;
-
     /**
      * Determine if the driver serves the request.
      *
      * @param  string $sitePath
      * @param  string $siteName
      * @param  string $uri
-     * @return boolean
+     * @return bool
      */
     public function serves($sitePath, $siteName, $uri)
     {
-        return file_exists($sitePath . '/bin/magento') && file_exists($sitePath . '/pub/index.php');
+        return file_exists($sitePath . '/pub/index.php') &&
+            file_exists($sitePath . '/bin/magento');
+    }
+
+    public function envExists($sitePath) {
+        return file_exists($sitePath.'/app/etc/env.php');
+    }
+
+    public function moduleConfigExists($sitePath) {
+        return file_exists($sitePath.'/app/etc/config.php');
+    }
+
+    public function installed($sitePath) {
+        return $this->envExists($sitePath) && $this->moduleConfigExists($sitePath);
     }
 
     /**
@@ -33,101 +38,41 @@ class Magento2ValetDriver extends ValetDriver
      */
     public function isStaticFile($sitePath, $siteName, $uri)
     {
-        $this->checkMageMode($sitePath);
+        $isMagentoStatic = false;
+        $resource = $uri;
 
-        $uri = $this->handleForVersions($uri);
-        $route = parse_url(substr($uri, 1))['path'];
-
-        $pub = '';
-        if ('developer' === $this->mageMode) {
-            $pub = 'pub/';
+        if(strpos($uri,'/pub') === 0 && file_exists($sitePath.'/setup'.$uri)) {
+            return $sitePath.'/setup'.$uri;
         }
 
-        if (!$this->isPubDirectory($sitePath, $route, $pub)) {
+        if (strpos($uri, '/static/') !== false) {
+            $isMagentoStatic = true;
+        }
+
+        if (!$isMagentoStatic && strpos($uri, '/media/') === false) {
             return false;
         }
 
-        $magentoPackagePubDir = $sitePath;
-        if ('developer' !== $this->mageMode) {
-            $magentoPackagePubDir .= '/pub';
+        if ($isMagentoStatic) {
+            $resource = preg_replace('#static(/version[0-9]+)?/#', '', $uri, 1);
+            $uri = '/static' . $resource;
         }
 
-        $file = $magentoPackagePubDir . '/' . $route;
-
-        if (file_exists($file)) {
-            return $magentoPackagePubDir . $uri;
+        if (file_exists($staticFilePath = $sitePath . '/pub' . $uri)) {
+            return $staticFilePath;
         }
 
-        if (strpos($route, $pub . 'static/') === 0) {
-            $route = preg_replace('#' . $pub . 'static/#', '', $route, 1);
-            $_GET['resource'] = $route;
-            include $magentoPackagePubDir . '/' . $pub . 'static.php';
+        if (strpos($uri, '/static/') === 0) {
+            $_GET['resource'] = $resource;
+            include($sitePath . DIRECTORY_SEPARATOR . 'pub' . DIRECTORY_SEPARATOR . 'static.php');
             exit;
         }
 
-        if (strpos($route, $pub . 'media/') === 0) {
-            include $magentoPackagePubDir . '/' . $pub . 'get.php';
+        if (strpos($uri, '/media/') === 0) {
+            include($sitePath . DIRECTORY_SEPARATOR . 'pub' . DIRECTORY_SEPARATOR . 'get.php');
             exit;
         }
 
-        return false;
-    }
-
-    /**
-     * Rewrite URLs that look like "versions12345/" to remove
-     * the versions12345/ part
-     *
-     * @param  string $route
-     */
-    private function handleForVersions($route)
-    {
-        return preg_replace('/version\d*\//', '', $route);
-    }
-
-    /**
-     * Determine the current MAGE_MODE
-     *
-     * @param  string $sitePath
-     */
-    private function checkMageMode($sitePath)
-    {
-        if (null !== $this->mageMode) {
-            // We have already figure out mode, no need to check it again
-            return;
-        }
-        if (!file_exists($sitePath . '/index.php')) {
-            $this->mageMode = 'production'; // Can't use developer mode without index.php in project root
-            return;
-        }
-        $mageConfig = [];
-        if (file_exists($sitePath . '/app/etc/env.php')) {
-            $mageConfig = require $sitePath . '/app/etc/env.php';
-        }
-        if (array_key_exists('MAGE_MODE', $mageConfig)) {
-            $this->mageMode = $mageConfig['MAGE_MODE'];
-        }
-    }
-
-    /**
-     * Checks to see if route is referencing any directory inside pub. This is a dynamic check so that if any new
-     * directories are added to pub this driver will not need to be updated.
-     *
-     * @param string $sitePath
-     * @param string $route
-     * @param string $pub
-     * @return bool
-     */
-    private function isPubDirectory($sitePath, $route, $pub = '')
-    {
-        $sitePath .= '/pub/';
-        $dirs = glob($sitePath . '*', GLOB_ONLYDIR);
-
-        $dirs = str_replace($sitePath, '', $dirs);
-        foreach ($dirs as $dir) {
-            if (strpos($route, $pub . $dir . '/') === 0) {
-                return true;
-            }
-        }
         return false;
     }
 
@@ -141,11 +86,36 @@ class Magento2ValetDriver extends ValetDriver
      */
     public function frontControllerPath($sitePath, $siteName, $uri)
     {
-        $this->checkMageMode($sitePath);
-
-        if ('developer' === $this->mageMode) {
-            return $sitePath . '/index.php';
+        if(isset($_GET['profile'])) {
+            $_SERVER['MAGE_PROFILER'] = 'html';
         }
+
+        if($uri === '/setup') {
+            Header('HTTP/1.1 301 Moved Permanently');
+            Header('Location: http://' . $_SERVER['HTTP_HOST'] . $uri . '/');
+            die;
+        }
+
+        if(strpos($uri, '/setup') === 0) {
+            $_SERVER['SCRIPT_FILENAME'] = $sitePath.'/setup/index.php';
+            $_SERVER['SCRIPT_NAME'] = '/index.php';
+            $_SERVER['DOCUMENT_ROOT'] = $sitePath.'/setup/';
+            $_SERVER['REQUEST_URI'] = str_replace('/setup', '', $_SERVER['REQUEST_URI']);
+
+            if($_SERVER['REQUEST_URI'] === '') {
+                $_SERVER['REQUEST_URI'] = '/';
+            }
+            return $sitePath.'/setup/index.php';
+        }
+
+        if(!$this->installed($sitePath)) {
+            http_response_code(404);
+            require __DIR__.'/../templates/magento2.php';
+            exit;
+        }
+
+        $_SERVER['DOCUMENT_ROOT'] = $sitePath;
+
         return $sitePath . '/pub/index.php';
     }
 }
---
+++ cli/templates/magento2.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+    <title>Valet - Not Found</title>
+
+    <style>
+        body {
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            font-size: 25px;
+            font-weight: 200;
+            line-height: 1.7;
+        }
+
+        h1 {
+            font-size: 40px;
+            font-weight: 200;
+        }
+
+        h2 {
+            font-weight: 200;
+        }
+
+        a {
+            color: #fff;
+            text-decoration: none;
+        }
+
+        span, .button {
+            background: #000;
+            color: #fff;
+            padding-left: 10px;
+            padding-right: 10px;
+            padding-bottom: 5px;
+            padding-top: 5px;
+        }
+    </style>
+</head>
+<body>
+<div>
+    <h1>Magento2 driver could not find <span>app/etc/config.php</span> and <span>app/etc/env.php</span></h1>
+    <p>New project? <a class="button" href="/setup">Open Magento2 setup</a></p>
+</div>
+</body>
+</html>
