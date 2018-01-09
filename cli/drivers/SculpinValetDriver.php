ec42860fee529bca94c09c2fbfab19314cd62c85
Beau Simensen <beau@dflydev.com>
2018-01-08 20:53:46 -0600

    Also check if sculpin/sculpin is required by composer.json

--- cli/drivers/SculpinValetDriver.php
+++ cli/drivers/SculpinValetDriver.php
@@ -18,12 +18,30 @@ class SculpinValetDriver extends BasicValetDriver
 
     private function isModernSculpinProject($sitePath)
     {
-        return is_dir($sitePath.'/source') && is_dir($sitePath.'/output_dev');
+        return is_dir($sitePath.'/source') &&
+            is_dir($sitePath.'/output_dev') &&
+            $this->composerRequiresSculpin($sitePath);
     }
 
     private function isLegacySculpinProject($sitePath)
     {
         return is_dir($sitePath.'/.sculpin');
+    }
+
+    private function composerRequiresSculpin($sitePath)
+    {
+        if (! file_exists($sitePath.'/composer.json')) {
+            return false;
+        }
+
+        $composer_json_source = file_get_contents($sitePath.'/composer.json');
+        $composer_json = json_decode($composer_json_source, true);
+
+        if (json_last_error() != JSON_ERROR_NONE) {
+            return false;
+        }
+
+        return isset($composer_json['require']['sculpin/sculpin']);
     }
 
     /**
