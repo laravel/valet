c9685e376ecf034af1be7e1d539490ef2dc23d1e
chejaziuhin <36676697+chejaziuhin@users.noreply.github.com>
2018-03-27 14:47:50 -0600

    Static file serving for subfolders
    This allows static file serving of 'index.php' and 'index.html' files from subfolders when you have a root 'public' folder.

--- cli/drivers/BasicValetDriver.php
+++ cli/drivers/BasicValetDriver.php
@@ -25,7 +25,11 @@ class BasicValetDriver extends ValetDriver
      */
     public function isStaticFile($sitePath, $siteName, $uri)
     {
-        if (file_exists($staticFilePath = $sitePath.'/public'.$uri)) {
+        if (file_exists($staticFilePath = $sitePath.'/public'.$uri.'index.html')) {
+            return $staticFilePath;
+        } elseif (file_exists($staticFilePath = $sitePath.'/public'.$uri.'index.php')) {
+            return $staticFilePath;
+        } elseif (file_exists($staticFilePath = $sitePath.'/public'.$uri)) {
             return $staticFilePath;
         } elseif ($this->isActualFile($staticFilePath = $sitePath.$uri)) {
             return $staticFilePath;
