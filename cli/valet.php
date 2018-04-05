74e203e37b9549b364feda1920e91e3156f38ad9
Adam Wathan <adam.wathan@gmail.com>
2018-04-05 08:44:49 -0400

    Bump version

--- cli/valet.php
+++ cli/valet.php
@@ -18,7 +18,7 @@ use Illuminate\Container\Container;
  */
 Container::setInstance(new Container);
 
-$version = '2.0.9';
+$version = '2.0.10';
 
 $app = new Application('Laravel Valet', $version);
 
