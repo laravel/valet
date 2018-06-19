e31522b623d283d3a9f4fea9148c401cae9a3fcf
Pablo Leal Rodríguez <p.leal@sofcloudit.com>
2018-06-19 12:25:45 +0200

    Fix for ignore port when trying to find the right folder.

--- server.php
+++ server.php
@@ -65,7 +65,7 @@ $uri = urldecode(
 
 $siteName = basename(
     // Filter host to support wildcard dns feature
-    valet_support_wildcard_dns($_SERVER['HTTP_HOST']),
+    valet_support_wildcard_dns(explode(':',$_SERVER['HTTP_HOST'])[0]),
     '.'.$valetConfig['domain']
 );
 
