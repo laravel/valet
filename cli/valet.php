9c7a64efd6632dccb088c016888202bdeaecb030
Matt Stauffer <matt@tighten.co>
2018-11-09 13:36:04 -0500

    Bump version

--- cli/valet.php
+++ cli/valet.php
@@ -29,7 +29,7 @@ if (is_dir(VALET_LEGACY_HOME_PATH) && !is_dir(VALET_HOME_PATH)) {
  */
 Container::setInstance(new Container);
 
-$version = '2.1.1';
+$version = '2.1.2';
 
 $app = new Application('Laravel Valet', $version);
 
