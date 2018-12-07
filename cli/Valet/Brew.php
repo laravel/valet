b3ce1c4a1505de4b3cbe2dae79bb3135c937a235
Till Krüss <tillkruss@users.noreply.github.com>
2018-12-07 12:55:11 -0800

    Support PHP 7.3

--- cli/Valet/Brew.php
+++ cli/Valet/Brew.php
@@ -7,8 +7,21 @@ use DomainException;
 
 class Brew
 {
-    const SUPPORTED_PHP_VERSIONS = ['php', 'php@7.2', 'php@7.1', 'php@7.0', 'php@5.6', 'php72', 'php71', 'php70', 'php56'];
-    const LATEST_PHP_VERSION = 'php@7.2';
+    const SUPPORTED_PHP_VERSIONS = [
+        'php',
+        'php@7.3',
+        'php@7.2',
+        'php@7.1',
+        'php@7.0',
+        'php@5.6',
+        'php73',
+        'php72',
+        'php71',
+        'php70',
+        'php56'
+    ];
+
+    const LATEST_PHP_VERSION = 'php@7.3';
 
     var $cli, $files;
 
