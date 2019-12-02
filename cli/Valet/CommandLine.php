deb3bf8ef012da9ac26cb56832cebaf4ad8a9be8
Filipe Craveiro <filipe@craveir0.com>
2019-12-02 14:01:41 +0000

    Symfony's 4.x Process component has deprecated passing a command string to the constructor, but older versions (which Valet's Composer constraints allow) don't have the fromShellCommandLine method. For more information, see: https://github.com/laravel/valet/pull/761

--- cli/Valet/CommandLine.php
+++ cli/Valet/CommandLine.php
@@ -75,7 +75,15 @@ class CommandLine
         $onError = $onError ?: function () {
         };
 
-        $process = new Process($command);
+        // Symfony's 4.x Process component has deprecated passing a command string
+        // to the constructor, but older versions (which Valet's Composer
+        // constraints allow) don't have the fromShellCommandLine method.
+        // For more information, see: https://github.com/laravel/valet/pull/761
+        if (method_exists(Process::class, 'fromShellCommandline')) {
+            $process = Process::fromShellCommandline($command);
+        } else {
+            $process = new Process($command);
+        }
 
         $processOutput = '';
         $process->setTimeout(null)->run(function ($type, $line) use (&$processOutput) {
