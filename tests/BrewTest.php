0b341a7ca11caaf3ad192bd0daaab777426f6aa1
Chris Brown <code@drbyte.dev>
2020-11-28 13:23:59 -0500

    Update test suite to phpunit 9.5
    Update test suite to phpunit 9.5 syntax
    Refactored to use polyfill for older PHP versions via `yoast/phpunit-polyfills`
    
    Note: this includes 2 important differences from usual phpunit test suites:
    - instead of extending `PHPUnit\Framework\TestCase` we extend `Yoast\PHPUnitPolyfills\TestCases\TestCase`
    - instead of handling fixtures via `setUp()` and `tearDown()` we use `set_up()` and `tear_down()` respectively
    
    Comment regarding formatting: I chose to use the FQDN in the `extends` syntax of the class declaration instead of using `use` so that it is more quickly apparent that we're doing something slightly different than usual phpunit syntax, particularly in regards to the set_up() / tear_down() methods that appear immediately following the `extends` line.

--- tests/BrewTest.php
+++ tests/BrewTest.php
@@ -9,27 +9,24 @@ use function Valet\swap;
 use Illuminate\Support\Collection;
 use Illuminate\Container\Container;
 
-class BrewTest extends PHPUnit_Framework_TestCase
+class BrewTest extends Yoast\PHPUnitPolyfills\TestCases\TestCase
 {
-    public function setUp()
+    public function set_up()
     {
         $_SERVER['SUDO_USER'] = user();
 
         Container::setInstance(new Container);
     }
 
-
-    public function tearDown()
+    public function tear_down()
     {
         Mockery::close();
     }
-
 
     public function test_brew_can_be_resolved_from_container()
     {
         $this->assertInstanceOf(Brew::class, resolve(Brew::class));
     }
-
 
     public function test_installed_returns_true_when_given_formula_is_installed()
     {
@@ -44,7 +41,6 @@ php@7.4');
         swap(CommandLine::class, $cli);
         $this->assertTrue(resolve(Brew::class)->installed('php'));
     }
-
 
     public function test_installed_returns_false_when_given_formula_is_not_installed()
     {
@@ -65,7 +61,6 @@ php7');
         swap(CommandLine::class, $cli);
         $this->assertFalse(resolve(Brew::class)->installed('php@7.4'));
     }
-
 
     public function test_has_installed_php_indicates_if_php_is_installed_via_brew()
     {
@@ -196,7 +191,6 @@ php7');
         $this->assertFalse($brew->hasInstalledPhp());
     }
 
-
     public function test_tap_taps_the_given_homebrew_repository()
     {
         $cli = Mockery::mock(CommandLine::class);
@@ -206,7 +200,6 @@ php7');
         swap(CommandLine::class, $cli);
         resolve(Brew::class)->tap('php@7.1', 'php@7.0', 'php@5.6');
     }
-
 
     public function test_restart_restarts_the_service_using_homebrew_services()
     {
@@ -218,7 +211,6 @@ php7');
         resolve(Brew::class)->restartService('dnsmasq');
     }
 
-
     public function test_stop_stops_the_service_using_homebrew_services()
     {
         $cli = Mockery::mock(CommandLine::class);
@@ -227,7 +219,6 @@ php7');
         swap(CommandLine::class, $cli);
         resolve(Brew::class)->stopService('dnsmasq');
     }
-
 
     public function test_linked_php_returns_linked_php_formula_name()
     {
@@ -262,17 +253,14 @@ php7');
         $this->assertSame('php@5.6', $getBrewMock($files)->linkedPhp());
     }
 
-
-    /**
-     * @expectedException DomainException
-     */
     public function test_linked_php_throws_exception_if_no_php_link()
     {
+        $this->expectException(DomainException::class);
+
         $brewMock = Mockery::mock(Brew::class)->makePartial();
         $brewMock->shouldReceive('hasLinkedPhp')->once()->andReturn(false);
         $brewMock->linkedPhp();
     }
-
 
     public function test_has_linked_php_returns_true_if_php_link_exists()
     {
@@ -285,12 +273,10 @@ php7');
         $this->assertTrue($brew->hasLinkedPhp());
     }
 
-
-    /**
-     * @expectedException DomainException
-     */
     public function test_linked_php_throws_exception_if_unsupported_php_version_is_linked()
     {
+        $this->expectException(DomainException::class);
+
         $files = Mockery::mock(Filesystem::class);
         $files->shouldReceive('isLink')->once()->with(BREW_PREFIX.'/bin/php')->andReturn(true);
         $files->shouldReceive('readLink')->once()->with(BREW_PREFIX.'/bin/php')->andReturn('/test/path/php/5.4.14/test');
@@ -298,15 +284,13 @@ php7');
         resolve(Brew::class)->linkedPhp();
     }
 
-
-    public function test_install_or_fail_will_install_brew_formulas()
+    public function test_install_or_fail_will_install_brew_formulae()
     {
         $cli = Mockery::mock(CommandLine::class);
         $cli->shouldReceive('runAsUser')->once()->with('brew install dnsmasq', Mockery::type('Closure'));
         swap(CommandLine::class, $cli);
         resolve(Brew::class)->installOrFail('dnsmasq');
     }
-
 
     public function test_install_or_fail_can_install_taps()
     {
@@ -318,12 +302,10 @@ php7');
         $brew->installOrFail('dnsmasq', [], ['test/tap']);
     }
 
-
-    /**
-     * @expectedException DomainException
-     */
     public function test_install_or_fail_throws_exception_on_failure()
     {
+        $this->expectException(DomainException::class);
+
         $cli = Mockery::mock(CommandLine::class);
         $cli->shouldReceive('runAsUser')->andReturnUsing(function ($command, $onError) {
             $onError(1, 'test error ouput');
@@ -332,11 +314,10 @@ php7');
         resolve(Brew::class)->installOrFail('dnsmasq');
     }
 
-    /**
-     * @expectedException DomainException
-     */
     public function test_link_will_throw_exception_on_failure()
     {
+        $this->expectException(DomainException::class);
+
         $cli = Mockery::mock(CommandLine::class);
         $cli->shouldReceive('runAsUser')->once()->withArgs([
             'brew link aformula',
@@ -372,11 +353,10 @@ php7');
         $this->assertSame('Some output forced', resolve(Brew::class)->link('aformula', true));
     }
 
-    /**
-     * @expectedException DomainException
-     */
     public function test_unlink_will_throw_exception_on_failure()
     {
+        $this->expectException(DomainException::class);
+
         $cli = Mockery::mock(CommandLine::class);
         $cli->shouldReceive('runAsUser')->once()->withArgs([
             'brew unlink aformula',
@@ -400,11 +380,10 @@ php7');
         $this->assertSame('Some output', resolve(Brew::class)->unlink('aformula'));
     }
 
-    /**
-     * @expectedException DomainException
-     */
     public function test_getRunningServices_will_throw_exception_on_failure()
     {
+        $this->expectException(DomainException::class);
+
         $cli = Mockery::mock(CommandLine::class);
         $cli->shouldReceive('runAsUser')->once()->withArgs([
             'brew services list | grep started | awk \'{ print $1; }\'',
@@ -485,48 +464,48 @@ php7');
     {
         return [
             [
-                '/test/path/php/7.3.0/test', // linked path
+                '/test/path/php/7.4.0/test', // linked path
                 [ // matches
-                    'path/php/7.3.0/test',
+                    'path/php/7.4.0/test',
                     'php',
                     '',
-                    '7.3',
+                    '7.4',
                     '.0',
                 ],
                 'php', // expected link formula
             ],
             [
-                '/test/path/php@7.2/7.2.13/test',
+                '/test/path/php@7.4/7.4.13/test',
                 [
-                    'path/php@7.2/7.2.13/test',
+                    'path/php@7.4/7.4.13/test',
                     'php',
-                    '@7.2',
-                    '7.2',
+                    '@7.4',
+                    '7.4',
                     '.13',
                 ],
-                'php@7.2'
+                'php@7.4'
             ],
             [
-                '/test/path/php/7.2.9_2/test',
+                '/test/path/php/7.4.9_2/test',
                 [
-                    'path/php/7.2.9_2/test',
+                    'path/php/7.4.9_2/test',
                     'php',
                     '',
-                    '7.2',
+                    '7.4',
                     '.9_2',
                 ],
                 'php',
             ],
             [
-                '/test/path/php72/7.2.9_2/test',
+                '/test/path/php74/7.4.9_2/test',
                 [
-                    'path/php72/7.2.9_2/test',
+                    'path/php74/7.4.9_2/test',
                     'php',
-                    '72',
-                    '7.2',
+                    '74',
+                    '7.4',
                     '.9_2',
                 ],
-                'php72',
+                'php74',
             ],
             [
                 '/test/path/php56/test',
