9d9b39bd68a03dca7320e3c69d581ee27f02f901
Dries Vints <dries@vints.io>
2022-05-24 16:54:47 +0200

    First attempt at testing CLI commands

---
+++ tests/CliTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Symfony\Component\Console\Tester\ApplicationTester;
+
+class CliTest extends  Yoast\PHPUnitPolyfills\TestCases\TestCase
+{
+    public function testParkCommands()
+    {
+        $application = require_once __DIR__ . '/../cli/app.php';
+        $application->setAutoExit(false);
+
+        $tester = new ApplicationTester($application);
+
+        $tester->run(['command' => 'park ./output']);
+        $tester->assertCommandIsSuccessful();
+
+        $output = $tester->getDisplay();
+        $this->assertStringContainsString("The [./output] directory has been added to Valet's paths.", $output);
+
+        $tester->run(['command' => 'parked']);
+        $tester->assertCommandIsSuccessful();
+
+        $output = $tester->getDisplay();
+        $this->assertStringContainsString('./output', $output);
+    }
+}
