6f1e4d421bfa7a206133e971a186de708c963adf
Dries Vints <dries@vints.io>
2022-09-08 13:42:09 +0200

    Update valet.php

--- cli/valet.php
+++ cli/valet.php
@@ -34,7 +34,7 @@ if (is_dir(VALET_LEGACY_HOME_PATH) && ! is_dir(VALET_HOME_PATH)) {
  */
 Container::setInstance(new Container);
 
-$version = '3.1.9';
+$version = '3.1.11';
 
 $app = new Application('Laravel Valet', $version);
 
