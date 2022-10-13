d54ac3206b96143f7b6fa36b9afdbf73b517c908
Joseph Richardson <j4h.r8n+github@gmail.com>
2022-10-12 22:25:21 -0400

    port over additional header information from other drivers

--- cli/drivers/BedrockValetDriver.php
+++ cli/drivers/BedrockValetDriver.php
@@ -48,6 +48,7 @@ class BedrockValetDriver extends BasicValetDriver
     public function frontControllerPath($sitePath, $siteName, $uri)
     {
         $_SERVER['PHP_SELF'] = $uri;
+        $_SERVER['SERVER_ADDR'] = '127.0.0.1';
         $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
 
         return parent::frontControllerPath(
