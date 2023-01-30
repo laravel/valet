6437faa72b32877b0c227c0f3f87b56a8c02d3bb
Matt Stauffer <matt@tighten.co>
2023-01-29 21:00:07 -0500

    Make 127.0.0.1 SERVER_ADDR definition only if null

--- cli/Valet/Drivers/WordPressValetDriver.php
+++ cli/Valet/Drivers/WordPressValetDriver.php
@@ -27,12 +27,10 @@ class WordPressValetDriver extends BasicValetDriver
      */
     public function frontControllerPath($sitePath, $siteName, $uri)
     {
-        $_SERVER['PHP_SELF'] = $uri;
-        $_SERVER['SERVER_ADDR'] = '127.0.0.1';
-        $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
-
         return parent::frontControllerPath(
-            $sitePath, $siteName, $this->forceTrailingSlash($uri)
+            $sitePath,
+            $siteName,
+            $this->forceTrailingSlash($uri)
         );
     }
 
