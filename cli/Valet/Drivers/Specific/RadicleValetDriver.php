b1b752abf826d11c44319f2f5545c2a1e797cf0a
Michael Utz <michael@theutz.local>
2023-05-02 11:03:13 -0400

    Add driver for Radicle by roots.io

---
+++ cli/Valet/Drivers/Specific/RadicleValetDriver.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Valet\Drivers;
+
+class RadicleValetDriver extends BasicValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return is_dir($sitePath.'/.radicle-setup');
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        $staticFilePath = $sitePath.'/public'.$uri;
+
+        if ($this->isActualFile($staticFilePath)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        return parent::frontControllerPath(
+            $sitePath.'/public',
+            $siteName,
+            $this->forceTrailingSlash($uri)
+        );
+    }
+
+    /**
+     * Redirect to uri with trailing slash.
+     *
+     * @param  string  $uri
+     * @return string
+     */
+    private function forceTrailingSlash($uri)
+    {
+        if (substr($uri, -1 * strlen('/wp/wp-admin')) == '/wp/wp-admin') {
+            header('Location: '.$uri.'/');
+            exit;
+        }
+
+        return $uri;
+    }
+}
