e52b9014158ebe55946383422788374a38f7a588
Jason Varga <jason@pixelfear.com>
2023-08-28 15:41:41 -0400

    add test for static caching

--- tests/Drivers/StatamicValetDriverTest.php
+++ tests/Drivers/StatamicValetDriverTest.php
@@ -32,4 +32,17 @@ class StatamicValetDriverTest extends BaseDriverTestCase
         $projectPath = $this->projectDir('statamic');
         $this->assertEquals($projectPath.'/public/index.php', $driver->frontControllerPath($projectPath, 'my-site', '/'));
     }
+
+    public function test_it_serves_statically_cached_pages()
+    {
+        $driver = new StatamicValetDriver();
+
+        $projectPath = $this->projectDir('statamic');
+
+        $_SERVER['REQUEST_URI'] = '/test';
+        $this->assertEquals($projectPath.'/public/static/test_.html', $driver->frontControllerPath($projectPath, 'my-site', '/test'));
+
+        $_SERVER['REQUEST_URI'] = '/test?foo=bar&baz=qux';
+        $this->assertEquals($projectPath.'/public/static/test_foo=bar&baz=qux.html', $driver->frontControllerPath($projectPath, 'my-site', '/test'));
+    }
 }
