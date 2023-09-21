03bb9cc131ffe47d73a06bc5f821b9485f0a6550
driesvints <driesvints@users.noreply.github.com>
2023-09-21 19:31:00 +0000

    Fix code styling

--- cli/app.php
+++ cli/app.php
@@ -117,7 +117,7 @@ if (is_dir(VALET_HOME_PATH)) {
             false
         );
 
-        if (false === $helper->ask($input, $output, $question)) {
+        if ($helper->ask($input, $output, $question) === false) {
             return warning('No new Valet tld was set.');
         }
 
@@ -409,7 +409,7 @@ if (is_dir(VALET_HOME_PATH)) {
             $helper = $this->getHelperSet()->get('question');
             $question = new ConfirmationQuestion('Would you like to install Expose now? [y/N] ', false);
 
-            if (false === $helper->ask($input, $output, $question)) {
+            if ($helper->ask($input, $output, $question) === false) {
                 info('Proceeding without installing Expose.');
 
                 return;
@@ -425,7 +425,7 @@ if (is_dir(VALET_HOME_PATH)) {
             $helper = $this->getHelperSet()->get('question');
             $question = new ConfirmationQuestion('Would you like to install ngrok via Homebrew now? [y/N] ', false);
 
-            if (false === $helper->ask($input, $output, $question)) {
+            if ($helper->ask($input, $output, $question) === false) {
                 info('Proceeding without installing ngrok.');
 
                 return;
@@ -540,7 +540,7 @@ if (is_dir(VALET_HOME_PATH)) {
             $helper = $this->getHelperSet()->get('question');
             $question = new ConfirmationQuestion('Are you sure you want to proceed? [y/N]', false);
 
-            if (false === $helper->ask($input, $output, $question)) {
+            if ($helper->ask($input, $output, $question) === false) {
                 return warning('Uninstall aborted.');
             }
 
