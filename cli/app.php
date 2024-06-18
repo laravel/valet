42ba0b20ad3001fd0d8f531c5276851eace00db3
Dries Vints <dries@vints.be>
2024-06-18 18:57:43 +0200

    version

--- cli/app.php
+++ cli/app.php
@@ -33,7 +33,7 @@ if (file_exists(__DIR__.'/../vendor/autoload.php')) {
  */
 Container::setInstance(new Container);
 
-$version = '4.6.3';
+$version = '4.7.0';
 
 $app = new Application('Laravel Valet', $version);
 
