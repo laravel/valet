1a0077a69d412f132e65a3e93fb8184510a39dd1
Matt Stauffer <matt@tighten.co>
2024-12-09 18:14:43 -0500

    Update version number for 4.8.2 release

--- cli/app.php
+++ cli/app.php
@@ -33,7 +33,7 @@ if (file_exists(__DIR__.'/../vendor/autoload.php')) {
  */
 Container::setInstance(new Container);
 
-$version = '4.8.0';
+$version = '4.8.2';
 
 $app = new Application('Laravel Valet', $version);
 
