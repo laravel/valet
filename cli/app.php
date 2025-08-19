18439215b6be1118c8b496604e5cd2d5aafddaad
Taylor Otwell <taylor@laravel.com>
2025-08-18 21:12:10 -0500

    Update app.php

--- cli/app.php
+++ cli/app.php
@@ -33,7 +33,7 @@ if (file_exists(__DIR__.'/../vendor/autoload.php')) {
  */
 Container::setInstance(new Container);
 
-$version = '4.9.0';
+$version = '4.9.1';
 
 $app = new Application('Laravel Valet', $version);
 
