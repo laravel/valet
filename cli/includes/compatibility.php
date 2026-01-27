3a22c9f60f1489003f25e7cbc7d4230943ebb3eb
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2026-01-27 15:50:50 +0300

    remove os

--- cli/includes/compatibility.php
+++ cli/includes/compatibility.php
@@ -10,12 +10,6 @@ if (php_sapi_name() !== 'cli') {
  */
 $inTestingEnvironment = strpos($_SERVER['SCRIPT_NAME'], 'phpunit') !== false;
 
-if (PHP_OS !== 'Darwin' && ! $inTestingEnvironment) {
-    echo 'Valet only supports the Mac operating system.'.PHP_EOL;
-
-    exit(1);
-}
-
 if (version_compare(PHP_VERSION, '8.0', '<')) {
     echo 'Valet requires PHP 8.0 or later.';
 
